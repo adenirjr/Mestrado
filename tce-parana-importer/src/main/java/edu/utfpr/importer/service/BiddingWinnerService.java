3518a7539c8c76cebfbd6cab0cc9e8f34f84ffed
a026710 <jr.adenir@gmail.com>
2017-09-24 19:00:33 -0300

    winner

---
+++ tce-parana-importer/src/main/java/edu/utfpr/importer/service/BiddingWinnerService.java
@@ -0,0 +1,51 @@
+package edu.utfpr.importer.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import edu.utfpr.importer.model.BiddingWinner;
+import edu.utfpr.importer.parser.SAXParser;
+import edu.utfpr.importer.persistence.provider.HibernateProvider;
+import edu.utfpr.importer.xml.handler.BiddingWinnerSAXHandler;
+
+public class BiddingWinnerService {
+
+	private final SAXParser<BiddingWinner> xmlParser = new SAXParser<BiddingWinner>();
+	private final EntityManager entityManager = HibernateProvider.getInstance().getEntityManager();
+	
+	private final EntityService entityService = new EntityService();
+
+	public void saveXMLContent(final String path, final String fileName) {
+		List<BiddingWinner> biddings = xmlParser.parseFile(path, fileName, BiddingWinnerSAXHandler.class);
+
+		if (biddings != null) {
+			save(biddings);
+		}
+	}
+
+	public void save(final List<BiddingWinner> winners) {
+		for (BiddingWinner winner : winners) {
+			save(winner);
+		}
+	}
+
+	public void save(final BiddingWinner winner) {
+		try {
+			entityService.save(winner.getPk().getEntity());
+			
+			entityManager.getTransaction().begin();
+			entityManager.persist(winner);
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			System.out.println("Bidding: " + winner.getPk().getBidding().getBidId() + " docNumber: " + winner.getPk().getEntity().getDocumentNumber() + " nrItem : " + winner.getPk().getItemNumber());
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		entityManager.close();
+	}
+}
---
+++ tce-parana-importer/src/main/java/edu/utfpr/importer/xml/handler/BiddingWinnerSAXHandler.java
@@ -0,0 +1,57 @@
+package edu.utfpr.importer.xml.handler;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import edu.utfpr.importer.model.Bidding;
+import edu.utfpr.importer.model.BiddingWinner;
+import edu.utfpr.importer.model.BiddingWinnerId;
+import edu.utfpr.importer.model.Corporate;
+import edu.utfpr.importer.model.Individual;
+
+public class BiddingWinnerSAXHandler extends GenericHandler<BiddingWinner> {
+
+	private BiddingWinner biddingWinner;
+
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		if (qName.equalsIgnoreCase("LicitacaoVencedor")) {
+			biddingWinner = new BiddingWinner();
+			biddingWinner.setPk(new BiddingWinnerId());
+			
+			final Bidding bidding = new Bidding();
+			bidding.setBidId(attributes.getValue("idlicitacao"));
+			biddingWinner.getPk().setBidding(bidding);
+			
+			biddingWinner.getPk().setItemNumber(attributes.getValue("nrItem"));
+			biddingWinner.getPk().setLotNumber(attributes.getValue("nrLote"));
+			biddingWinner.setAmount(attributes.getValue("nrQuantidadeVencedorLicitacao"));
+			biddingWinner.setItemPrice(attributes.getValue("vlLicitacaoVencedorLicitacao"));
+			biddingWinner.setDescItem(attributes.getValue("dsItem"));
+
+			String documentNumber = attributes.getValue("nrDocumento");
+			
+			if(documentNumber != null) {
+			    documentNumber = documentNumber.replace(".", "").replace("-", "").replace("/", "");
+			}
+
+			if (documentNumber.length() <= 11) {
+				final Individual individualEntity = new Individual();
+				individualEntity.setName(attributes.getValue("nmPessoa"));
+				individualEntity.setDocumentNumber(documentNumber);
+				biddingWinner.getPk().setEntity(individualEntity);
+			} else {
+				final Corporate corporateEntity = new Corporate();
+				corporateEntity.setName(attributes.getValue("nmPessoa"));
+				corporateEntity.setDocumentNumber(documentNumber);
+				biddingWinner.getPk().setEntity(corporateEntity);
+			}
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if (qName.equalsIgnoreCase("LicitacaoVencedor")) {
+			addParsedData(biddingWinner);
+		}
+	}
+}
